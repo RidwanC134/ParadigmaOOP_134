0e6f03508f2d63b2e4b44e389e0306da5b9a1c1a
RidwanC134 <ridwanhh10@gmail.com>
2023-05-22 17:58:54 +0700

    Membuat conductor untuk dokter

--- Asosiasi_134/Asosiasi_134.cpp
+++ Asosiasi_134/Asosiasi_134.cpp
@@ -21,4 +21,11 @@ class dokter {
 public:
 	string nama;
 	vector<pasien*> daftar_pasien;
+
+	dokter(string pNama) :nama(pNama) {
+		cout << "Dokter \"" << nama << "\" ada\n";
+	}
+	~dokter() {
+		cout << "Dokter \"" << nama "\" tidak ada\n";
+	}
 };

56dfc451523c739c3c0c74429282d288c2a82148
RidwanC134 <ridwanhh10@gmail.com>
2023-05-22 18:44:29 +0700

    Membuat conductor ibu

--- mainIA_134/ibu.h
+++ mainIA_134/ibu.h
@@ -6,5 +6,10 @@ public:
 	string nama;
 	vector<anak*> daftar_anak;
 
-	
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "Ibu \"" << nama << "\" tidak\n";
+	}
 };

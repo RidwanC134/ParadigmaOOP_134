f8a500e4c75a9976de658d702af6a984425c0956
RidwanC134 <ridwanhh10@gmail.com>
2023-05-22 20:25:40 +0700

    Membuat conductor untuk mahasiswa

--- memberinitlist_134/memberinitlist_134.cpp
+++ memberinitlist_134/memberinitlist_134.cpp
@@ -4,10 +4,18 @@ using namespace std;
 
 class mahasiswa {
 private:
-	const int id:
-	string nama:
+	const int id :
+	string nama :
 	float nilai;
 
 public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nilai(pNilai) { //Member Initialization list}
+		//Definisi
+	}
 
+	~mahasiswa() {
+		cout << "Id = " << id << endl;
+		cout << "Nama = " << nama << endl;
+		cout << "Nilai = " << nilai << endl;
+	}
 };
